3520fd88cc615152b4ec99dfd94edab43a567b18
Juan <39202665+ajuancer@users.noreply.github.com>
2024-03-29 18:48:03 +0100

    Chapters 16 and 17

--- VideoTema01/src/IteratorIF.java
+++ VideoTema01/src/IteratorIF.java
@@ -1,4 +1,11 @@
 
-public interface IteratorIF {
+public interface IteratorIF<E> {
+	boolean hasNext ();
+	
+	Object next( );
 
+	/*
+	 * @pre next ( ) has been called multiple times
+	 */
+	void remove( );
 }
